16b5156e67c19ace574eeffd484d73b52042177a
Muata Nkosi <mnkosi@fandm.edu>
2018-04-18 21:44:34 -0400

    Finished course

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -3,6 +3,7 @@ package com.pluralsight.controller;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +12,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pluralsight.model.Activity;
 import com.pluralsight.model.Exercise;
+import com.pluralsight.service.ExerciseService;
 
 @Controller
 public class MinutesController {
 	
+	
+	@Autowired
+	private ExerciseService exerciseService; 
+	
 	@RequestMapping(value="/addMinutes")
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
-		System.out.println("exercise: " + exercise.getMinutes());
 		
+		System.out.println("exercise: " + exercise.getMinutes());
+		System.out.println("exercise: " + exercise.getActivity());
 		return "addMinutes";
 		//return "forward:addMoreMinutes.html"; sends value to next 
 		//return "redirect:addMoreMinutes.html"; //ends request and starts new on
@@ -26,21 +33,9 @@ public class MinutesController {
 	
 	@RequestMapping(value="/activities", method=RequestMethod.GET)
 	public @ResponseBody List<Activity> findAllActivities() {
-		List<Activity> activities = new ArrayList<Activity>();
 		
-		Activity run = new Activity();
-		run.setDesc("Run");
-		activities.add(run);
 		
-		Activity bike = new Activity();
-		bike.setDesc("Bike");
-		activities.add(bike);
-		
-		Activity swim = new Activity();
-		swim.setDesc("Swim");
-		activities.add(swim);
-		
-		return activities;
+		return exerciseService.findAllActivities();
 	}
 	
 //	@RequestMapping(value="/addMinutes")
